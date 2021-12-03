877f78f891e4e3abc2478c0d1c8d6748d606b03d
kouhei-sugahara <200092tw@yse-c.net>
2021-12-03 09:16:51 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+//require('dotenv').config()
+dotenv.config()
+const host = process.env.HOST
+const port = process.env.PORT
+const default_login_name = process.env.LOGIN_NAME
+const default_password = process.env.PASSWORD
+
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+app.use(express.urlencoded({extended: true}))
+
+app.post('/auth',(req,res) => {
+    let message = 'ログインできません'
+    const login_name = req.body.login_name
+    const password = req.body.password
+    console.log(login_name)
+    console.log(password)
+
+    if(login_name == default_login_name
+        && password == default_password){
+            message = 'ログインしました'
+        }
+    res.send(message)
+})
+
+app.get('/', (req,res) => {
+    res.send('Hello YSE!!!!')
+})
+
+app.get('/profile',(req,res) => {
+    res.send('prifileOK')
+})
+
+app.post('/update',(req,res) => {
+    res.send('更新しました')
+})
+
+app.listen(port,host, () => {
+    console.log('http://' + host + ':' + port)
+})
+
